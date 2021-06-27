c4bc3b83542f61035041c72eb993269dba461a63
kemalekren <kemalekren@hotmail.com>
2021-06-27 22:00:43 +0300

    Game VC Reload logic and alert view added

--- TicTacToe/GameController/GameVC.swift
+++ TicTacToe/GameController/GameVC.swift
@@ -12,6 +12,8 @@ final class GameVC: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var playerLabel: UILabel!
     
+    private let flowLayout = UICollectionViewFlowLayout()
+    
     private let gameCell = "gameCell"
     private let grid = 9
     
@@ -21,12 +23,14 @@ final class GameVC: UIViewController {
         }
     }
     
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let flowLayout = UICollectionViewFlowLayout()
-        
+        setupViews()
+    }
+    
+    private func setupViews() {
+        navigationItem.title = "Tic Tac Toe"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "New Game", style: .plain, target: self, action: #selector(reloadGame))
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.collectionViewLayout = flowLayout
@@ -41,13 +45,14 @@ extension GameVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.cellForItem(at: indexPath) as! GameCell
+            let cell = collectionView.cellForItem(at: indexPath) as! GameCell
         
-        if !cell.isCellUsed {
-          
-            cell.player = vm.checkScore(indexPath: indexPath.row)
-            cell.isCellUsed = true
-        }
+            if !cell.isCellUsed {
+                let result = self.vm.checkScore(indexPath: indexPath.row)
+                cell.player = result.player
+                playerLabel.text = result.nextTurn
+                cell.isCellUsed = true
+            }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -65,15 +70,35 @@ extension GameVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 2
     }
+    
+    @objc private func reloadGame() {
+        vm.resetGame()
+    }
 }
 
 extension GameVC: GameVMOutputDelegate {
+    func startNewGame() {
+        playerLabel.text = "Player One"
+        collectionView.reloadData()
+    }
+    
     func gameResult(player: Player?, result: GameResult) {
         switch result {
         case .win:
-            print("\(player!.playerName) is Win")
+            if let player = player {
+                showAlertViewWith(message: "\(player.playerType.rawValue) is Win")
+            }
+           
         case .tie:
-            print("Game is tie")
+            showAlertViewWith(message: "Game is tie")
         }
+    }
+    
+    private func showAlertViewWith(message: String){
+        let alert = UIAlertController(title: "Game Over", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { action in
+            self.reloadGame()
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 }
